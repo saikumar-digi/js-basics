a96209f73d630e141e1c5c01df408cbbcc6d0f98
saikumar <saikumar@digisprint.com>
2024-01-22 21:27:02 +0530

    added filter map reduce

--- basic.js
+++ basic.js
@@ -56,3 +56,44 @@ const myResult1 = arr.forEach((ar,i)=>{
 console.log(myResult1);
 
 console.log(myResult , arr);
+
+
+// forEach example
+const numbers = [1, 2, 3, 4, 5];
+
+console.log("Original array:", numbers);
+
+// Using forEach to double each element in the array
+numbers.forEach(function (number, index, array) {
+  array[index] = number * 2;
+});
+
+console.log("Array after using forEach:", numbers);
+
+// map example
+const numbers1 = [1, 2, 3, 4, 5];
+
+console.log("Original array:", numbers1);
+
+// Using map to create a new array with each element doubled
+const doubledNumbers = numbers1.map(function (number) {
+  return number * 2;
+});
+
+console.log("New array created with map:", doubledNumbers);
+
+
+// Using filter to get only even numbers
+const evenNumbers = numbers.filter(function (number) {
+  return number % 2 === 0;
+});
+
+console.log("Even numbers:", evenNumbers);
+
+// Using reduce to calculate the sum of squared even numbers
+const sumOfSquaredEvenNumbers = evenNumbers.reduce(function (accumulator, number) {
+  return accumulator + (number * number);
+}, 0);
+
+console.log("Sum of squared even numbers:", sumOfSquaredEvenNumbers);
+
